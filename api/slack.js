ef2fda0deeb6351d70d5dea981c0fa16de4e813e
bengHak <qkwl4678@naver.com>
2020-12-27 11:55:07 +0900

    get username, commit link, timestamp

--- api/slack.js
+++ api/slack.js
@@ -1,9 +1,11 @@
 const Slack = require('slack-node');
 const schedule = require('node-schedule');
-
 const CONFIG = require('../config/config');
 
 let slack = new Slack(CONFIG.slack_api_token)
+
+// TODO: get last commit
+// TODO: 
 
 const readCommitHistory = () => slack.api(
     'conversations.history',
@@ -12,8 +14,34 @@ const readCommitHistory = () => slack.api(
         limit: 1000,
     },
     (e, res) => {
-        console.log(res.messages[0]['attachments'][0]['text']);
-        // console.log(res[0]['bot_profile']);
+        let messages = res.messages
+            .map(e => {
+                if (e['attachments'] === undefined)
+                    return null;
+                return {
+                    ts: e['ts'],
+                    detail: e['attachments'][0]
+                }
+            })
+            // Exclude simple messages (ex. someone joined)
+            .filter(e => e !== null)
+            // Exclude pr, creating repo
+            .filter(e => e['detail']['author_name'] !== undefined && e['detail']['text'] !== undefined)
+            .map(e => {
+                const commit_text = e['detail']['text'];
+                let commit_link = commit_text.substring(
+                    commit_text.indexOf('<') + 1,
+                    commit_text.indexOf('|')
+                )
+                const offset = new Date().getTimezoneOffset();
+                const ts = new Date((e['ts'] * 1000) - (offset * 60 * 1000));
+                return {
+                    author_name: e['detail']['author_name'],
+                    text: commit_link,
+                    timestamp: ts
+                };
+            });
+        console.log(messages);
     }
 )
 
