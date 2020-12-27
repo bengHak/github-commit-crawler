fea33e0e9867150aa032f7c37908a27abb7ebc9c
bengHak <qkwl4678@naver.com>
2020-12-27 17:40:19 +0900

    update db functions

--- api/db.js
+++ api/db.js
@@ -1,9 +1,93 @@
-const { Client } = require('pg');
+const { Pool } = require('pg');
+const { member_list_github } = require('../config/config');
+const CONFIG = require('../config/config');
 
-const client = new Client({
-    user: 'DB 사용자 명',
-    host: 'DB 주소',
-    database: 'DB명',
-    password: '비밀번호',
-    port: 5432,
+const pool = new Pool({
+  user: CONFIG.db_user,
+  host: CONFIG.db_host,
+  database: CONFIG.db_name,
+  password: CONFIG.db_password,
+  port: CONFIG.db_port,
 });
+
+const getCommits = async () => {
+  let query = await pool.query('SELECT * FROM COMMIT_LOG');
+  return query['rows'];
+};
+
+const getLastSavedCommitTime = async () => {
+  let query = await pool.query(
+    'SELECT * FROM COMMIT_LOG ORDER BY CREATED_ON DESC LIMIT 1'
+  );
+  if (query['rows'].length === 0) return new Date('1996-03-18');
+  return query['rows'][0]['created_on'];
+};
+
+const getYesterdayComitter = async () => {
+  let query = await pool.query(
+    'SELECT * FROM COMMIT_LOG WHERE DATE(created_on) = current_date-1'
+  );
+
+  let commit = [];
+  let notCommit = CONFIG.member_list;
+  const username_list = CONFIG.member_list_github;
+
+  query['rows'].map((e) => {
+    const idx = username_list.indexOf(e['github_username']);
+    if (commit.includes(notCommit[idx])) return;
+    commit.push(notCommit[idx]);
+  });
+
+  notCommit = notCommit.filter((e) => !commit.includes(e));
+
+  return {
+    commit,
+    notCommit,
+  };
+};
+
+const getTodayComitter = async () => {
+  let query = await pool.query(
+    'SELECT * FROM COMMIT_LOG WHERE DATE(created_on) = current_date'
+  );
+
+  let commit = [];
+  let notCommit = CONFIG.member_list;
+  const username_list = CONFIG.member_list_github;
+
+  query['rows'].map((e) => {
+    const idx = username_list.indexOf(e['github_username']);
+    if (commit.includes(notCommit[idx])) return;
+    commit.push(notCommit[idx]);
+  });
+
+  notCommit = notCommit.filter((e) => !commit.includes(e));
+
+  return {
+    commit,
+    notCommit,
+  };
+};
+
+const saveCommit = async ({ username, commitLink, timestamp }) => {
+  pool.query(
+    'INSERT INTO COMMIT_LOG (github_username, commit_link, created_on) VALUES ($1, $2, $3)',
+    [username, commitLink, timestamp],
+    (error, results) => {
+      if (error) {
+        console.log(error);
+      }
+      console.log(results);
+    }
+  );
+};
+
+getYesterdayComitter();
+
+module.exports = {
+  getCommits,
+  getLastSavedCommitTime,
+  getYesterdayComitter,
+  getTodayComitter,
+  saveCommit,
+};
