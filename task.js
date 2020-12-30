a5d981fef8fa1d354de2f8dd482c97ac9559cb7a
bengHak <qkwl4678@naver.com>
2020-12-30 19:24:21 +0900

    [fix] edit crawling period

--- task.js
+++ task.js
@@ -1,20 +1,16 @@
 const schedule = require('node-schedule');
-const {
-  getLastSavedCommitTime,
-  saveCommit,
-  getTodayComitter,
-} = require('./api/db');
+const { getLastSavedCommitTime, saveCommit } = require('./api/db');
 const { sendYesterdayResult, sendTodayResult } = require('./api/kakaowork');
 const { getUnsavedCommit } = require('./api/slack');
 
 // 오후 10시에 현황 보내기
-schedule.scheduleJob('0 22 * * * *', () => sendTodayResult());
+schedule.scheduleJob('1 22 * * *', () => sendTodayResult());
 
 // 오전 10시에 어제 결과 보내기
-schedule.scheduleJob('0 10 * * *', () => sendYesterdayResult());
+schedule.scheduleJob('1 10 * * *', () => sendYesterdayResult());
 
 // DB에 저장되지 않은 커밋 저장
-schedule.scheduleJob('*/30 * * * *', async function () {
+schedule.scheduleJob('0 * * * *', async function () {
   const lastTime = await getLastSavedCommitTime().then((res) => res);
   let unSavedCommit = await getUnsavedCommit(lastTime);
   unSavedCommit = unSavedCommit.reverse();
